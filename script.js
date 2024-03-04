3ca3229dab54b0dedfc15850d629dcc4eab99390
atanuray2001 <147434373+atanuray2001@users.noreply.github.com>
2024-03-04 18:51:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const lenis = new Lenis()
+let nav = document.querySelector("nav");
+let ham = document.querySelector(".ham");
+let lll = 1;
 
-lenis.on('scroll', (e) => {
-  console.log(e)
+ham.addEventListener("click",function(){
+
+  if(lll==1){
+    nav.style.visibility = "visible";
+    nav.style.opacity = 1;
+    lll = 0
+  }
+  else {
+    nav.style.visibility = "hidden";
+    nav.style.opacity = 0;
+    lll = 1
+  }
+
 })
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
